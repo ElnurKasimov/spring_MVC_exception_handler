789d845bd1dacee4c0f460ffa14f454aa63647de
Elnur Kasimov <ekasimov2012@gmail.com>
2023-10-03 18:07:44 +0300

    deleted unuseable code from  ApplicationExceptionHandler

--- src/main/java/com/softserve/itacademy/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/softserve/itacademy/exception/ApplicationExceptionHandler.java
@@ -1,13 +1,10 @@
 package com.softserve.itacademy.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.server.ResponseStatusException;
+
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
